c95e99dd8a8be79ec9195eb8b6501bb66e2291d7
xDestyn <omar.flores.cs@outlook.com>
2022-02-25 09:11:53 -0600

    Add function app url + update local.settings.json

--- NeighborlyFrontEnd/settings.py
+++ NeighborlyFrontEnd/settings.py
@@ -16,4 +16,4 @@ API_VERSION = 'v1'
 #API_URL = " https://neighborlyapi.azurewebsites.net/api/"
 
 # for local host if Azure functions served locally
-API_URL = "http://localhost:7071/api"
+API_URL = "https://proj2-function-app.azurewebsites.net/api"
